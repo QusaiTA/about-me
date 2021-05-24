d218e9680a0664470a7ee2e2719efadfc2880f9e
QusaiTA <tashtoshqusai@gmail.com>
2021-05-24 13:37:49 +0300

    making about me demo

---
+++ js/app.js
@@ -0,0 +1,86 @@
+'use strict';
+
+
+let userName = prompt('What is your name ?');
+console.log(userName);
+alert('Thank u for visiting my website, i hope u will have much fun' + " " + userName);
+
+let likeSports = prompt('did u think i like sport or not ?').toUpperCase();
+
+console.log(likeSports);
+
+if(likeSports==='YES'){
+    alert('no u are wrong :P ');
+
+}else if(likeSports === 'NO') {
+    alert('yes u are right <3');
+
+}
+else{
+    alert('U need to answer with YES/NO ')
+}
+
+
+let likeFastFood =prompt('did u think i like FastFood or not ?').toUpperCase();
+
+console.log(likeFastFood);
+
+
+if(likeFastFood==='YES'){
+    alert('yes u are so much right <3 ');
+
+}else if(likeFastFood === 'NO') {
+    alert('no u are wrong :( ');
+
+}
+else{
+    alert('U need to answer with YES/NO ')
+}
+
+let likeClassicClothes = prompt('did u think i like ClassicClothes or not ?').toUpperCase();
+
+console.log(likeClassicClothes);
+
+if(likeClassicClothes==='YES'){
+    alert('no u are wrong :( ');
+
+}else if(likeClassicClothes === 'NO') {
+    alert('yes u are right <3 ');
+
+}
+else{
+    alert('U need to answer with YES/NO ')
+}
+
+let likeFruits = prompt('did u think i like Fruits or not ?').toUpperCase();
+
+console.log(likeFruits);
+
+if(likeFruits==='YES'){
+    alert('yes u are right <3 ');
+
+}else if(likeFruits === 'NO') {
+    alert('no u are wrong :( ');
+
+}
+else{
+    alert('U need to answer with YES/NO ')
+}
+
+let stayingAtHome = prompt('did u think i like staying at home or not ?').toUpperCase();
+
+console.log(stayingAtHome);
+
+if(stayingAtHome==='YES'){
+    alert('yes u are right <3 ');
+
+}else if(stayingAtHome === 'NO') {
+    alert('no u are wrong :( ');
+
+}
+else{
+    alert('U need to answer with YES/NO ')
+}
+
+
+alert('Thanks again for visiting i hope u enjoyed so much with the guessing game, u are most welcomed to visit again ' + " " + userName);
